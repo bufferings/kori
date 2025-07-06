366cc2ed7db53a8e09474ac1f7679773baffb173
Mitsuyuki Shiiba <bufferings@gmail.com>
2025-07-06 21:32:34 +0900

    feat: add http header constants and typed methods
    - Add HttpRequestHeader and HttpResponseHeader constants for major headers
    - Add getHeader method to KoriRequest with HttpRequestHeaderValue type
    - Update setHeader, appendHeader, removeHeader in KoriResponse with HttpResponseHeaderValue type
    - Improve type safety and IDE autocompletion for header manipulation

---
+++ packages/kori/src/http/headers.ts
@@ -0,0 +1,43 @@
+/**
+ * HTTP Request Header constants
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers#request_headers
+ */
+export const HttpRequestHeader = {
+  ACCEPT: 'accept',
+  ACCEPT_CHARSET: 'accept-charset',
+  ACCEPT_ENCODING: 'accept-encoding',
+  ACCEPT_LANGUAGE: 'accept-language',
+  AUTHORIZATION: 'authorization',
+  CACHE_CONTROL: 'cache-control',
+  CONTENT_TYPE: 'content-type',
+  COOKIE: 'cookie',
+  FORWARDED: 'forwarded',
+  FROM: 'from',
+  HOST: 'host',
+  USER_AGENT: 'user-agent',
+  X_FORWARDED_FOR: 'x-forwarded-for',
+  X_FORWARDED_HOST: 'x-forwarded-host',
+  X_FORWARDED_PROTO: 'x-forwarded-proto',
+} as const;
+
+export type HttpRequestHeaderValue = (typeof HttpRequestHeader)[keyof typeof HttpRequestHeader] | (string & {});
+
+/**
+ * HTTP Response Header constants
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers#response_headers
+ */
+export const HttpResponseHeader = {
+  ACCESS_CONTROL_ALLOW_ORIGIN: 'access-control-allow-origin',
+  CONTENT_ENCODING: 'content-encoding',
+  CONTENT_TYPE: 'content-type',
+  ETAG: 'etag',
+  LOCATION: 'location',
+  SET_COOKIE: 'set-cookie',
+  STRICT_TRANSPORT_SECURITY: 'strict-transport-security',
+  VARY: 'vary',
+  WWW_AUTHENTICATE: 'www-authenticate',
+} as const;
+
+export type HttpResponseHeaderValue = (typeof HttpResponseHeader)[keyof typeof HttpResponseHeader] | (string & {});
--- packages/kori/src/http/index.ts
+++ packages/kori/src/http/index.ts
@@ -1,3 +1,9 @@
 export { ContentType, type ContentTypeValue, DEFAULT_CONTENT_TYPE } from './content-types.js';
+export {
+  HttpRequestHeader,
+  type HttpRequestHeaderValue,
+  HttpResponseHeader,
+  type HttpResponseHeaderValue,
+} from './headers.js';
 export { getMethodString } from './method.js';
 export { HttpStatus, type HttpStatusCode } from './status-codes.js';
