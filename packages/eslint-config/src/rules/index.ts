ff0664497dab60cf9fc1e140c20a929acc78e2e2
Mitsuyuki Shiiba <bufferings@gmail.com>
2025-08-05 01:17:20 +0900

    Add no-barrel-internal ESLint rule and TSDoc guidelines

--- packages/eslint-config/src/rules/index.ts
+++ packages/eslint-config/src/rules/index.ts
@@ -1,3 +1,4 @@
 export { asciiOnlySource } from './ascii-only-source.js';
+export { noBarrelInternal } from './no-barrel-internal.js';
 export { noDuplicateExportFrom } from './no-duplicate-export-from.js';
 export { noIndexImports } from './no-index-imports.js';
